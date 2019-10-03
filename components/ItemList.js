835434ec451c12bada407ae8eb4e7ba87789a751
Bennett Dungan <bennettdungan@gmail.com>
2019-10-02 21:06:21 -0500

    more verbose return on ItemList

--- components/ItemList.js
+++ components/ItemList.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const ItemList = ({ items, deleteItem }) => (
-  <ul>
-    {items.map((item, index) => (
-        <li>
-          <h2>{item}</h2>
-          <button onClick={()=> deleteItem(index)}>
-            Delete Item
-          </button>
-        </li>
-    ))}
-  </ul>
-)
+const ItemList = ({ items, deleteItem }) => {
+
+  return (
+    <ul>
+      {items.map((item, index) => (
+          <li>
+            <h2>{item}</h2>
+            <button onClick={()=> deleteItem(index)}>
+              Delete Item
+            </button>
+          </li>
+      ))}
+    </ul>
+  )
+}
 
 export default ItemList;
