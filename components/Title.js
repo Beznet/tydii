754d9b9e4b2914745cb143bca61c891294005a2a
Bennett Dungan <bennettdungan@gmail.com>
2020-01-25 15:08:50 -0600

    title animation added

--- components/Title.js
+++ components/Title.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Title = ({ copy, role }) => (
-  <span aria-label={copy} role={role}>
-    {copy.split('').map((char, index) => (
-      <span aria-hidden="true" key={index}>
-        {char}
-      </span>
-    ))}
-  </span>
+  <h1 aria-label={copy} role={role}>
+    {copy.split('').map(function(char, index) {
+      const incomingText = { 'animation-delay': `${0.5 + index / 10}s` };
+      return (
+        <span aria-hidden="true" key={index} style={incomingText}>
+          {char}
+        </span>
+      );
+    })}
+  </h1>
 );
 
 export default Title;
