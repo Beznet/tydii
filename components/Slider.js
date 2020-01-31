3307d90248f06764e76ef95fd8dc4def2574eddf
Bennett Dungan <bennettdungan@gmail.com>
2020-01-30 23:06:19 -0600

    added bounce effect to slider image update

--- components/Slider.js
+++ components/Slider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useEffect, useRef } from 'react';
 
 const faceImages = [
   '',
@@ -10,14 +10,18 @@ const faceImages = [
 ];
 
 const Slider = ({ value, onValueChange }) => {
+  const ref = useRef()
   const onChange = e => {
     onValueChange(e.target.value);
+    ref.current.classList.add("bounce")
+    setTimeout(()=> ref.current.classList.remove("bounce"), 200)
   };
 
   return (
     <>
       <input type="range" value={value} onChange={onChange} min="1" max="5" />
       <img
+        ref={ref}
         className="face-img pt-n2"
         src={faceImages[value]}
         alt="face-rating"
