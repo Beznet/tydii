4f4ef20c874db2da567d847bf48708250f6c67a5
Bennett Dungan <bennettdungan@gmail.com>
2020-04-05 23:29:03 -0500

    add dotenv and users, me, auth api routes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// Load appropriate env file if possible
+try {
+  let envPath = '.env'
+
+  if (process.env.CI_COMMIT_REF_NAME) {
+    envPath = `.${process.env.CI_COMMIT_REF_NAME}.env`
+  }
+
+  require('dotenv').config({ path: envPath })
+} catch (error) {}
+
 // next.config.js
 const withCSS = require('@zeit/next-css');
 
@@ -15,3 +26,4 @@ module.exports = withCSS({
     ],
   },
 });
+
