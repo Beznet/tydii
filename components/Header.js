4ec575bbeb59ccaafb4c3c9c8ea80af3e8ae3570
Bennett Dungan <bennettdungan@gmail.com>
2020-04-06 23:58:38 -0500

    login and signup components added

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,8 @@ const Header = () => (
     <Col sm="3"></Col>
     <Col className="text-center" sm="6">
       <Title copy="Tydii" role="heading" />
+      Login
+      Sign Up
     </Col>
     <Col sm="3"></Col>
   </Row>
