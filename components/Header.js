b6340bd6c0b01a712569153dd499afa73ff6c674
Bennett Dungan <bennettdungan@gmail.com>
2020-04-08 23:22:17 -0500

    added Login component with modal

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
-import { Col, Row } from 'reactstrap';
+import { Col, Row, Button } from 'reactstrap';
 import Title from './Title';
+import LoginForm from './login';
 
 const Header = () => (
   <Row className="mb-2 mt-sm-2">
     <Col sm="3"></Col>
     <Col className="text-center" sm="6">
       <Title copy="Tydii" role="heading" />
-      Login
-      Sign Up
+      <LoginForm />
     </Col>
     <Col sm="3"></Col>
   </Row>
--- components/Login.js
+++ components/Login.js
@@ -1,58 +1,104 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
+import {
+  Button,
+  Input,
+  Form,
+  FormGroup,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from 'reactstrap'
 import Router from 'next/router';
 import cookie from 'js-cookie';
+import useToggle from '../hooks/useToggle'
 
-const Login = () => {
-  const [loginError, setLoginError] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function LoginForm () {
+  const [LoginModalOpen, toggleLoginModal] = useToggle()
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    //call api
-    fetch('/api/auth', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    })
-      .then((r) => {
-        return r.json();
+  function LoginModal ({open, toggle}) {
+    const [loginError, setLoginError] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleCloseClick = useCallback(() => {
+      toggle()
+    }, [])
+
+    function handleSubmit(e) {
+      e.preventDefault();
+      //call api
+      fetch('/api/auth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
       })
-      .then((data) => {
-        if (data && data.error) {
-          setLoginError(data.message);
-        }
-        if (data && data.token) {
-          //set cookie
-          cookie.set('token', data.token, {expires: 2});
-          Router.push('/');
-        }
-      });
-  }
+        .then((r) => {
+          return r.json();
+        })
+        .then((data) => {
+          if (data && data.error) {
+            setLoginError(data.message);
+          }
+          if (data && data.token) {
+            //set cookie
+            cookie.set('token', data.token, {expires: 2});
+            Router.push('/');
+          }
+        });
+      toggle()
+    }
+    
+    return (
+      <Modal
+      isOpen={open}
+      toggle={handleCloseClick}
+      modalTransition={{ timeout: 0 }}
+      backdropTransition={{ timeout: 0 }}
+      >
+        <Form onSubmit={handleSubmit}>
+          <ModalHeader tag='h2' close={<i className='close fa fa-close cursor-pointer' onClick={handleCloseClick} />}>
+            Login
+          </ModalHeader>
+          <ModalBody>
+          <FormGroup>
+            <Input
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <ModalFooter>
+              <Input type="submit" value="Submit" />
+            </ModalFooter>
+            {loginError && <p style={{color: 'red'}}>{loginError}</p>}
+          </FormGroup>
+          </ModalBody>
+        </Form>
+      </Modal>
+    );
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <p>Login</p>
-      <input
-        name="email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+    <>
+      <Button className='m-2' onClick={toggleLoginModal} color='success' size='lg'>Login</Button>
+      <LoginModal
+        open={LoginModalOpen}
+        toggle={toggleLoginModal}
       />
-      <input
-        name="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input type="submit" value="Submit" />
-      {loginError && <p style={{color: 'red'}}>{loginError}</p>}
-    </form>
-  );
+    </>
+  )
 };
 
-export default Login;
+export default LoginForm;
