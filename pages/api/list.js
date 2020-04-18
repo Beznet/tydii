bf6577dd7f7a563193ffa92ae63a2dd0d00bf6bd
Bennett Dungan <bennettdungan@gmail.com>
2020-04-18 00:24:14 -0500

    basic crappy POST working

--- pages/api/list.js
+++ pages/api/list.js
@@ -6,34 +6,24 @@ const client = new MongoClient(process.env.URL, {
     useUnifiedTopology: true,
   });
 
-function insertList(db, list) {
+async function insertList(db, list) {
     const collection = db.collection('user');
-    collection.insertOne({list}, function(err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        })
+    let result = await collection.insertOne({list})
+    return result
 }
 
-export default (req, res) => {
-    if (req.method === 'POST') {
-      client.connect(function() {
-        console.log('Connected to MongoDB server =>');
-        const db = client.db(process.env.DB_NAME);
-        const list = req.body.list;
-  
-        insertList(db, list, function(err) {
-            if (err) {
-            res.status(500).json({error: true, message: 'Error creating list'});
-            return;
-            }
-            if (!list) {
-            res.status(404).json({error: true, message: 'User not found'});
-            return;
-            } else {
-            res.status(200).json({list})
-            return;
-            }
-        });
-      });
-    }
-  };
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    client.connect(function() {
+
+      console.log('Connected to MongoDB server =>')
+
+      const db = client.db(process.env.DB_NAME)
+      const list = req.body.list
+      
+      insertList(db, list)
+      res.status(201).json({ success: true, data: list})
+      return
+    });
+  }
+};
