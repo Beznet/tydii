1195fdcf53393469d08acd04ca4d38e08b8a8a25
Bennett Dungan <bennettdungan@gmail.com>
2020-04-28 21:36:15 -0500

    persisted state hook added

---
+++ hooks/usePersistedState.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function usePersistedState(key, defaultValue) {
+    const [state, setState] = React.useState(
+      () => JSON.parse(localStorage.getItem(key)) || defaultValue
+    );
+    useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(state));
+    }, [key, state]);
+    return [state, setState];
+  }
+
+  export default usePersistedState
