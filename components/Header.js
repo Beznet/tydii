5d20217283179b1f71ba36f5780d5b43d739bbba
Bennett Dungan <bennettdungan@gmail.com>
2020-05-04 23:23:25 -0500

    added link to home on title click

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,14 @@
 import { Col, Row } from 'reactstrap';
 import Title from './Title';
+import Link from 'next/link';
 
 const Header = () => (
   <Row className="mb-2 mt-sm-2">
     <Col sm="3"></Col>
     <Col className="text-center" sm="6">
-      <Title copy="Tydii" role="heading" />
+      <Link href='/' >
+        <a className='text-decoration-none'><Title copy="Tydii" role="heading" /></a>
+      </Link>
     </Col>
     <Col sm="3">
     </Col>
