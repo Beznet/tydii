cb470c80f3128d312b5dd3ff511d9b8a94c010bd
Bennett Dungan <bennettdungan@gmail.com>
2020-05-04 21:16:25 -0500

    divided items into keep and donate arrays and renders them under respective categories

--- pages/results.js
+++ pages/results.js
@@ -2,21 +2,30 @@ import Layout from '../components/Layout';
 import React from 'react'
 import usePersistedState from '../hooks/usePersistedState'
 
-const LocalStateResults = () => {
+export default function LocalStateResults () {
   const [localStateItems, _] = usePersistedState('items', {})
+  const localStateValues = Object.values(localStateItems)
+  const donate = localStateValues.filter( item => item.rating <= 3)
+  const keep = localStateValues.filter( item => item.rating > 3)
 
+
+
+
+  // todo: take each item name/rating and list it into different categories
   return (
-    <div>
-      {JSON.stringify(localStateItems)}
-    </div>
-  )
-
-}
-
-export default function Results() {
-    return (
     <Layout>
-        <LocalStateResults />
+      <div>
+        <h2>Keep</h2>
+        {
+          keep.map( item => <li>{item.name}</li>)
+        }
+      </div>
+      <div>
+        <h2>Donate</h2>
+        {
+          donate.map( item => <li>{item.name}</li>)
+        }
+      </div>
     </Layout>
-    );
+  )
 }
