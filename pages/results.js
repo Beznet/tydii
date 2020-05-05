039a5100b6cfa9d6e6f4f34f6511d5101dd398b6
Bennett Dungan <bennettdungan@gmail.com>
2020-05-04 23:16:33 -0500

    save to database functionality added to /results page

--- pages/results.js
+++ pages/results.js
@@ -1,12 +1,39 @@
-import Layout from '../components/Layout';
+import Layout from '../components/Layout'
 import React from 'react'
 import usePersistedState from '../hooks/usePersistedState'
+import useSWR from 'swr'
 
 export default function LocalStateResults () {
   const [localStateItems, _] = usePersistedState('items', {})
   const localStateValues = Object.values(localStateItems)
   const donate = localStateValues.filter( item => item.rating <= 3)
   const keep = localStateValues.filter( item => item.rating > 3)
+
+  const {data} = useSWR('/api/me', async function(args) {
+    const res = await fetch(args);
+    return res.json();
+  });
+  if(!data) {
+    console.log('no data')
+  }
+  else if (data.userId) {
+    console.log('logged in')
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch('/api/list', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: data.userId,
+        list: localStateItems
+      }),
+    })
+      .then((r) => r.json())
+  }
 
   return (
     <Layout>
@@ -22,6 +49,7 @@ export default function LocalStateResults () {
           donate.map( item => <li>{item.name}</li>)
         }
       </div>
+      <button type='submit' onClick={handleSubmit}>Save</button>
     </Layout>
   )
 }
