61451065fba5b34e83252691b530221470f4fd83
Bennett Dungan <bennettdungan@gmail.com>
2020-05-06 23:19:18 -0500

    added basic alert if someone isn't logged in and tries to save list

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,5 +1,4 @@
 import React, {useState, useCallback} from 'react';
-import Router from 'next/router';
 import {
   Input,
   Label,
--- pages/results.js
+++ pages/results.js
@@ -2,23 +2,29 @@ import Layout from '../components/Layout'
 import React from 'react'
 import usePersistedState from '../hooks/usePersistedState'
 import useSWR from 'swr'
+import useToggle from '../hooks/useToggle'
 
 export default function LocalStateResults () {
   const [localStateItems, _] = usePersistedState('items', {})
   const localStateValues = Object.values(localStateItems)
   const donate = localStateValues.filter( item => item.rating <= 3)
   const keep = localStateValues.filter( item => item.rating > 3)
+  let loggedIn = false
 
   const {data} = useSWR('/api/me', async function(args) {
     const res = await fetch(args);
     return res.json();
   });
   if(!data) {
+    loggedIn = false
     console.log('no data')
   }
   else if (data.userId) {
+    loggedIn = true
     console.log('logged in')
   }
+
+  console.log(loggedIn)
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -50,6 +56,11 @@ export default function LocalStateResults () {
         }
       </div>
       <button type='submit' onClick={handleSubmit}>Save</button>
+      {!loggedIn &&
+        <div class="alert alert-warning" role="alert">
+          Sign up or Login to save list
+        </div>
+      }
     </Layout>
   )
 }
