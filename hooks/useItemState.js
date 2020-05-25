e16a125c8933a4162009398886ebb5ba7f534510
Bennett Dungan <bennettdungan@gmail.com>
2020-05-25 13:34:36 -0500

    segmented the donate/sell, sold and donated categories into their own functional components. Added item text animation

--- hooks/useItemState.js
+++ hooks/useItemState.js
@@ -44,6 +44,7 @@ export default initialValue => {
       setItems([...items, generateNewItem(itemText)])
     },
     updateItem: updatedItem => {
+      console.log('UPDATED ITEM', updatedItem)
       const updatedItemList = updateItem(items, updatedItem)
       setItems(updatedItemList)
     },
--- pages/results.js
+++ pages/results.js
@@ -2,46 +2,87 @@ import Layout from '../components/Layout'
 import React from 'react'
 import usePersistedState from '../hooks/usePersistedState'
 import {
-  Col, 
+  Col,
   Row,
-  UncontrolledDropdown, 
-  DropdownToggle, 
-  DropdownMenu, 
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
   DropdownItem
 } from 'reactstrap'
 import useSWR from 'swr'
 import useItemState from '../hooks/useItemState'
 
-const DonateSellDropdown = ({updateItem, item}) => {
+const DonateSellDropdown = ({ updateItem, item }) => {
+
   const handleClick = (e) => {
     const result = e.target.value
     updateItem(Object.assign({}, item, { result }))
   }
 
-  const dropDownTitle = item.result
+  const dropdownTitle = item.result
 
   return (
-    <UncontrolledDropdown>
-    <DropdownToggle caret>
-      {dropDownTitle}
-    </DropdownToggle>
-    <DropdownMenu>
-      <DropdownItem value='sold' onClick={handleClick}>Sold</DropdownItem>
-      <DropdownItem value='donated' onClick={handleClick}>Donated</DropdownItem>
-    </DropdownMenu>
-  </UncontrolledDropdown>
+    <UncontrolledDropdown group size='sm' >
+      <DropdownToggle caret>
+        {dropdownTitle}
+      </DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem value='owned' onClick={handleClick}>owned</DropdownItem>
+        <DropdownItem value='sold' onClick={handleClick}>sold</DropdownItem>
+        <DropdownItem value='donated' onClick={handleClick}>donated</DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
   )
 }
 
-export default function LocalStateResults () {
+const DonateSellResults = ({ items, updateItem, handleSubmit }) => (
+  <Col lg="4" md="2">
+    <h2>Donate or Sell</h2>
+    {
+      items.map(item => (
+        <Row className="d-flex-inline mb-2">
+          <Col className="d-flex-inline" xs='6'>
+            <h5>
+              {item.result !== 'owned' ? <s>{item.name}</s> : item.name}
+            </h5>
+          </Col>
+          <Col className="d-flex-inline" xs='6'>
+            <DonateSellDropdown updateItem={updateItem} item={item} />
+          </Col>
+        </Row>
+      ))
+    }
+    <button type='submit' onClick={handleSubmit}>Save</button>
+  </Col>
+)
+
+const DonatedItems = ({ items }) => (
+  <Col lg="4" md="2">
+    <h2>Donated Items</h2>
+    {
+      items.map(item => item.result === 'donated' && <h5 className='item'>{item.name}</h5>)
+    }
+  </Col>
+)
+
+const SoldItems = ({ items }) => (
+  <Col lg="4" md="2">
+    <h2>Sold Items</h2>
+    {
+      items.map(item => item.result === 'sold' && <h5 className='item'>{item.name}</h5>)
+    }
+  </Col>
+)
+
+export default function LocalStateResults() {
   const [localStateItems, _] = usePersistedState('items', {})
   const localStateValues = Object.values(localStateItems)
-  const filteredLocalItems = localStateValues.filter( item => item.rating <= 3)
+  const filteredLocalItems = localStateValues.filter(item => item.rating <= 3)
   const { items, deleteItem, updateItem } = useItemState(filteredLocalItems)
-  const {data} = useSWR('/api/me', async function(args) {
+  const { data } = useSWR('/api/me', async function (args) {
     const res = await fetch(args)
     const data = res.json()
-  
+
     return data;
   })
 
@@ -60,29 +101,15 @@ export default function LocalStateResults () {
       .then((r) => r.json())
   }
 
-  console.log(items)
+  console.log(data)
 
   return (
     <Layout>
       <Row>
-        <Col lg="4" md="2">
-          <h2>Donate or Sell</h2>
-          {
-            items.map( item => 
-              <li>
-                {item.name}
-                <DonateSellDropdown updateItem={updateItem} item={item} />
-              </li>)
-          }
-        <button type='submit' onClick={handleSubmit}>Save</button>
-        </Col>
-        <Col lg="4" md="2">
-          <h2>Donate Bucket</h2>
-        </Col>
-        <Col lg="4" md="2">
-          <h2>Sell Bucket</h2>
-          </Col>
-        </Row>
+        <DonateSellResults items={items} updateItem={updateItem} handleSubmit={handleSubmit} />
+        <DonatedItems items={items} />
+        <SoldItems items={items} />
+      </Row>
     </Layout>
   )
 }
