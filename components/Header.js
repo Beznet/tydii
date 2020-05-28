cb4689ca0c6ef53f6fc1295a35a9d32c4bf30a9c
Bennett Dungan <bennettdungan@gmail.com>
2020-05-27 23:36:38 -0500

    remove home link on title click

--- components/Header.js
+++ components/Header.js
@@ -23,9 +23,7 @@ const Header = () => {
   <Row className="mb-2 mt-sm-2">
     <Col sm="3"></Col>
     <Col className="text-center" sm="6">
-      <Link href='/' >
-        <a className='text-decoration-none'><Title copy="Tydii" role="heading" /></a>
-      </Link>
+      <Title copy="Tydii" role="heading" />
     </Col>
     <Col sm="3">
     {loggedIn &&
