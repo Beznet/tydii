59fb8b5979bd9fe35e0196127ab3747274ab567a
Bennett Dungan <bennettdungan@gmail.com>
2020-05-30 21:11:14 -0500

    added welcome message above logout button

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,10 @@ import cookie from 'js-cookie'
 import useSWR from 'swr'
 
 const Header = () => {
-  const {data, revalidate} = useSWR('/api/me', async function(args) {
+  const { data, revalidate } = useSWR('/api/me', async function (args) {
     const res = await fetch(args)
     const data = res.json()
-    
+
     return data;
   })
   let loggedIn = false
@@ -19,29 +19,34 @@ const Header = () => {
     loggedIn = true;
   }
 
-  return(
-  <Row className="mb-2 mt-sm-2">
-    <Col sm="3"></Col>
-    <Col className="text-center" sm="6">
-      <Title copy="Tydii" role="heading" />
-    </Col>
-    <Col sm="3">
-    {loggedIn &&
-          <Link href='/'>
-            <a>
-              <button
-                onClick={() => {
-                  cookie.remove('token')
-                  revalidate()
-                }}
-              >
-                Logout
-              </button>
-            </a>
-          </Link>
-          }
-    </Col>
-  </Row>
+  return (
+    <Row className="mb-2 mt-sm-2">
+      <Col sm="3"></Col>
+      <Col className="text-center" sm="6">
+        <Title copy="Tydii" role="heading" />
+      </Col>
+      <Col sm="3">
+        {loggedIn &&
+          <Row>
+            <Col className='d-flex flex-column'>
+              <span>Welcome back!</span>
+              <Link href='/'>
+                <a>
+                  <button
+                    onClick={() => {
+                      cookie.remove('token')
+                      revalidate()
+                    }}
+                  >
+                    Logout
+                  </button>
+                </a>
+              </Link>
+            </Col>
+          </Row>
+        }
+      </Col>
+    </Row>
   )
 }
 
